d50563ee4b023b1cdc7030168e6dc89741c17776
Brian Jesse <brian.jesse@gmail.com>
2013-07-01 10:07:43 -0500

    added analytics code

--- src/gittalk/views/layout.clj
+++ src/gittalk/views/layout.clj
@@ -37,6 +37,7 @@
          [:div.reveal
            [:div.slides
              [:section {:data-markdown (add-context "/md/gittalk.md") :data-seperator "^\\n---\\n$" :data-vertical "^\\n--\\n$"}]]]
+         [:script "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','//www.google-analytics.com/analytics.js','ga'); ga('create', 'UA-34998188-2', 'brian-jesse.com'); ga('send', 'pageview');"]
          [:script
            {:src "//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"}]
          (include-js "head.min.js" "reveal.js" "bootstrap.min.js" "app.js")]]))
